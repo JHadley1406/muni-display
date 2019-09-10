7b34fc5a9d95c8602e539d97d9ce237a41a3ce98
Josiah Hadley <josiah.hadley@gmail.com>
2019-09-10 10:48:23 -0700

    refactor: Code cleanup

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Col from 'react-bootstrap/lib/Col';
+import Well from 'react-bootstrap/lib/Well';
 
 class Messages extends React.Component{
 
     displayMessages = () =>{
-        this.props.messages.map(function(message){
-            return(<div>{message}</div>);
-        })
+        return(this.props.messages.map(function(message){
+            return(<div key={message.id}><Well>{message.text}</Well></div>);
+        }));
     };
 
     render(){
-        return(<div></div>);
+        return(<Col md={12} style={{background: 'lightgrey', border: '2px solid black', borderRadius: '5px'}}>
+            {this.displayMessages()}
+            </Col>);
     }
 
 }
